5d80a52102700c4c18c274575bcf442a347c7c41
Mysterion186 <kowsikan@hotmail.fr>
2021-12-20 14:51:23 +0100

    ajout de commentaire

--- exo3/client.c
+++ exo3/client.c
@@ -13,7 +13,6 @@ extern int errno;
 
 int main(int argc, char* argv[])
 {
-
 	int S1 = socket(AF_INET, SOCK_STREAM,0); // création de la socket 
 	// remplissage des champs de la socket 
 	struct sockaddr_in Ad1;
@@ -27,7 +26,6 @@ int main(int argc, char* argv[])
 		if(errno==EBADF){printf("Pb bind : mauvais descripteur");}
 		if(errno==EINVAL){printf("Socket deja lie à une adresse");}
 	}
-	
     // remplissage des informations de serveur avec les infos passé en paramètre
 	struct sockaddr_in Ad_dest;
 	Ad_dest.sin_family = AF_INET;
@@ -36,10 +34,8 @@ int main(int argc, char* argv[])
 	hp = gethostbyname(argv[1]); // on obtient l'adresse IP via le nom de la machine
 	if(hp == NULL){return -1;}
 	memcpy(&Ad_dest.sin_addr,hp->h_addr_list[0],hp->h_length); // copie de l'adresse IP dans la struct 
-
     // partie connect pour le client
 	int connect_client = connect(S1,(struct sockaddr *) &Ad_dest,sizeof(Ad_dest));
-
     // requete à envoyer
     char requete[100] = "GET /";
     char *file_name = argv[3]; // nom du fichier demandé
@@ -49,7 +45,6 @@ int main(int argc, char* argv[])
     char reponse[1200];
 	printf("%s",requete);
     write(S1,requete,sizeof(requete));
-
     // lecture de ce que l'on reçoit
     read(S1,reponse,sizeof(reponse));
     printf("%s",reponse);
